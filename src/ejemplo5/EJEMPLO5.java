4b071204ea6e19c467757c385f78477dac3eaf2f
Javier Velasquez <progra@gmail.com>
2024-01-19 11:16:26 -0600

    primer repositorio

---
+++ src/ejemplo5/EJEMPLO5.java
@@ -0,0 +1,16 @@
+
+package ejemplo5;
+
+public class EJEMPLO5 {
+
+    public static void main(String[] args) {
+       
+        byte entero;
+        
+        entero = 110;
+            
+        System.out.println("Numero entero: "+ entero);
+        
+    }
+    
+}
